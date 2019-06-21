fb627bf69134365d191681f724f971ecdb2a9d86
Abhay Sharma <abhaysharmawebd@gmail.com>
2019-06-21 01:27:45 -0400

    added AXIOS to use API saving data to Firebase

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -9,6 +9,9 @@
 import Todo from "@/components/Todo.vue";
 import TodoForm from "@/components/TodoForm.vue";
 
+// We will simply need to import axios since it a node package we do not need to specify a file.
+import axios from "axios";
+
 export default {
   components: {
     Todo,
@@ -16,16 +19,50 @@ export default {
   },
   data() {
     return {
-      todoList: ["Walk the dogs", "Go for a ride"]
+      todoList: []
     };
   },
   methods: {
     appDeleteTodo(index) {
       this.todoList.splice(index, 1);
+      axios.put(
+        "https://vue-and-axios.firebaseio.com/data.json",
+        this.todoList
+      );
     },
     addTodo(todo) {
       this.todoList.push(todo);
+
+      // Making an axios put request to send the todoList array to Firebase
+      axios
+        .put("https://vue-and-axios.firebaseio.com/data.json", this.todoList)
+        .then(response => {
+          // we are logging the status message on success
+          console.log("Your data was saved status: " + response.status);
+        })
+        .catch(error => {
+          // we are logging the error message
+          console.log("There was an error in saving data: " + error.response);
+        });
     }
+  },
+  // created is a life cycle hook which will run when the instance is created
+  created() {
+    // here we are making a GET request using AXIOS to get the data
+    axios
+      .get("https://vue-and-axios.firebaseio.com/data.json")
+      .then(response => {
+        // console.log(response);
+        // console.log(response.data);
+
+        // Checking if the response has some data
+        if (response.data) {
+          this.todoList = response.data;
+        }
+      })
+      .catch(error => {
+        console.log("There was an error in getting data: " + error.response);
+      });
   }
 };
 </script>
